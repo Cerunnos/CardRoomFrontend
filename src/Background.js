157a4d6832fb94bae95fed138698ab45b4682c82
Cerunnos <github email address>
2018-08-29 12:22:45 -0400

    Added Face Cards

--- src/Background.js
+++ src/Background.js
@@ -12,16 +12,46 @@ class Background extends Component {
   state={
     deck:[],
     discard:[],
-    hand:[]
+    hand:[],
+    selectedCard:null
   }
 
   createDeck=()=>{
     let newDeck=[]
     for (let i=0;i<13;i++){
-      let cardSpade={number:i+1,suite:"Spades"}
-      let cardClub={number:i+1,suite:"Clubs"}
-      let cardDiamond={number:i+1,suite:"Diamonds"}
-      let cardHeart={number:i+1,suite:"Hearts"}
+      let cardSpade={}
+      let cardClub={}
+      let cardDiamond={}
+      let cardHeart={}
+      if (i+1===1){
+        cardSpade={number:"Ace",suite:"of Spades"}
+        cardClub={number:"Ace",suite:"of Clubs"}
+        cardDiamond={number:"Ace",suite:"of Diamonds"}
+        cardHeart={number:"Ace",suite:"of Hearts"}
+      }
+      else if (i+1===11){
+        cardSpade={number:"Jack",suite:"of Spades"}
+        cardClub={number:"Jack",suite:"of Clubs"}
+        cardDiamond={number:"Jack",suite:"of Diamonds"}
+        cardHeart={number:"Jack",suite:"of Hearts"}
+      }
+      else if (i+1===12){
+        cardSpade={number:"Queen",suite:"of Spades"}
+        cardClub={number:"Queen",suite:"of Clubs"}
+        cardDiamond={number:"Queen",suite:"of Diamonds"}
+        cardHeart={number:"Queen",suite:"of Hearts"}
+      }
+      else if (i+1===13){
+        cardSpade={number:"King",suite:"of Spades"}
+        cardClub={number:"King",suite:"of Clubs"}
+        cardDiamond={number:"King",suite:"of Diamonds"}
+        cardHeart={number:"King",suite:"of Hearts"}
+      }else{
+        cardSpade={number:i+1,suite:"of Spades"}
+        cardClub={number:i+1,suite:"of Clubs"}
+        cardDiamond={number:i+1,suite:"of Diamonds"}
+        cardHeart={number:i+1,suite:"of Hearts"}
+      }
       newDeck.push(cardSpade)
       newDeck.push(cardClub)
       newDeck.push(cardDiamond)
@@ -29,6 +59,12 @@ class Background extends Component {
     }
     shuffleDeck(newDeck)
     this.props.dispatch(setDeck(newDeck))
+  }
+
+  selectCard=(card)=>{
+    this.setState({
+      selectedCard:card
+    },()=>console.log(this.state.selectedCard))
   }
 
 
@@ -60,7 +96,7 @@ class Background extends Component {
       <div className="Background">
         <Deck drawCard={this.drawCard}/>
         <Discard/>
-        <Hand/>
+        <Hand selectCard={this.selectCard}/>
       </div>
     );
   }
--- src/Card.js
+++ src/Card.js
@@ -4,9 +4,12 @@ import './Card.css';
 import {connect} from 'react-redux'
 
 class Card extends Component {
+  handleClick=()=>{
+    this.props.selectCard(this.props.card)
+  }
   render() {
     return (
-      <div className="Card">
+      <div className="Card" onClick={this.handleClick}>
         <h4>{this.props.card.number}</h4>
         <p>{this.props.card.suite}</p>
       </div>
